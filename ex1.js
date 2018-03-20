396537f495fbb1799fd2cce8f8c9c973f7ec4e45
Patrick Sollars <pjsollars@gmail.com>
2018-03-20 13:19:21 -0400

    adds one more test and code cleanup

--- ex1.js
+++ ex1.js
@@ -2,7 +2,6 @@
 
 // Given an array of numbers, sort ascending odd numbers but leave even numbers in their original place in the array.  Zero does not need to move. If the array is empty, return an empty array.
 
-
 // Examples:
 // [5, 3, 2, 8, 1, 4] should return [1, 3, 2, 8, 5, 4]
 // [6, 3, 2, 7, 5, 0] should return [6, 3, 2, 5, 7, 0]
@@ -33,11 +32,9 @@ function oddSort(array) {
     if (number === 1) {
       sortedArray.push(newOddArray[0]);
       newOddArray = newOddArray.slice(1);
-      console.log(newOddArray);
     } else {
       sortedArray.push(newEvenArray[0]);
       newEvenArray = newEvenArray.slice(1);
-      console.log(newEvenArray);
     }
   });
   
@@ -46,8 +43,9 @@ function oddSort(array) {
 
 const quickTest1 = oddSort([5, 3, 2, 8, 1, 4]);
 console.log(quickTest1);
-console.log(quickTest1 === [ 1, 3, 2, 8, 5, 4 ]);
 
 const quickTest2 = oddSort([6, 3, 2, 7, 5, 0]);
 console.log(quickTest2);
-console.log(quickTest2 === [ 6, 3, 2, 5, 7, 0 ]);
+
+const quickTest3 = oddSort([]);
+console.log(quickTest3);
