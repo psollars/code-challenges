bd18b79fa6bd87e593be95600b611dc22d6c253b
Patrick Sollars <pjsollars@gmail.com>
2018-03-20 12:57:22 -0400

    adds test for ex1

--- ex1.js
+++ ex1.js
@@ -16,3 +16,11 @@ function oddSort(array) {
   // assemble new array back together
 
 }
+
+const quickTest1 = oddSort([5, 3, 2, 8, 1, 4]);
+console.log(quickTest1);
+console.log(quickTest1 === [1, 3, 2, 8, 5, 4]);
+
+const quickTest2 = oddSort([6, 3, 2, 7, 5, 0]);
+console.log(quickTest2);
+console.log(quickTest2 === [6, 3, 2, 5, 7, 0]);
